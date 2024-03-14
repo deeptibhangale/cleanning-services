3b643baffd9bbe34dad2aa477d53835eaea97159
deeptibhangale <deeptibhangale@gmail.com>
2024-03-14 11:41:07 -0700

    all changes done for adding and retrieving

--- src/main/java/com/MyJpa/CleanningServices/cleanningservices/controller/ClientController.java
+++ src/main/java/com/MyJpa/CleanningServices/cleanningservices/controller/ClientController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 public class ClientController {
     @Autowired
+
     private ClientService clientService;
     @GetMapping("/clients")
     public List<ClientDetails> getAllClients(){
